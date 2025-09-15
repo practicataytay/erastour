caa2a9f35348374ed0a2a8b8cc2ae2f5514d8144
Maria Fernanda Sanchez Arauz <a2024111238@estudiantes.upsa.edu.bo>
2025-09-14 21:14:53 -0400

    mateo

--- ErasTourAPI/Data/AppDbContext.cs
+++ ErasTourAPI/Data/AppDbContext.cs
@@ -1,6 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using ErasTourAPI.Models;
+
 namespace ErasTourAPI.Data;
 
-public class AppDbContext
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
-    
+    public DbSet<Album1989> Albums1989 { get; set; }
 }
